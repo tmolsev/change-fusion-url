ad50ef94c1d8ec70ea770f9c2be43a6840b28a8c
valahul <30555482+valahul@users.noreply.github.com>
2023-09-05 15:09:57 +0300

    Update theme-change.js

--- theme-change.js
+++ theme-change.js
@@ -1,7 +1,7 @@
 function change_URL() {
   const role = scriptElement.getAttribute('user_role');
   switch (role) {
-    case "Sales":
+    case "FND_APPLICATION_DEVELOPER_JOB":
       //window.location.replace("http://www.w3schools.com");
       console.log(" FOUND-REDIRECT")
       break;
