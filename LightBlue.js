3bebc98faae49dcd976166499d61c0a68b7c8530
valahul <30555482+valahul@users.noreply.github.com>
2023-09-05 15:41:56 +0300

    Update LightBlue.js

--- LightBlue.js
+++ LightBlue.js
@@ -1,9 +1,15 @@
-
 function change_URL() {
-
-      const fullURL = window.location.href;
-      console.log(fullURL);
       
-     // window.location.replace("https://fa-eqjw-dev10-saasfademo1.ds-fa.oraclepdemos.com/crmUI/faces/FuseWelcome?fndThemeName=Light%20Blue");
+const fullURL = window.location.href;
+console.log(fullURL);
+      
+if (fullURL.includes("fndThemeName=Light Blue")) {
+  console.log("The string contains 'fndThemeName=Light Blue'");
+} else {
+  console.log("The string does not contain 'fndThemeName=Light Blue'");
+      window.location.replace("https://fa-eqjw-dev10-saasfademo1.ds-fa.oraclepdemos.com/crmUI/faces/FuseWelcome?fndThemeName=Light%20Blue");
+}
+
+      
 }
  change_URL()
