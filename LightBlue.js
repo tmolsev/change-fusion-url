b929e09beb3fc429896a8ae315c874835cd67ce7
valahul <30555482+valahul@users.noreply.github.com>
2023-09-05 15:20:07 +0300

    Update LightBlue.js

--- LightBlue.js
+++ LightBlue.js
@@ -1,1 +1,5 @@
-https://fa-eqjw-dev10-saasfademo1.ds-fa.oraclepdemos.com/crmUI/faces/FuseWelcome?fndThemeName=Light%20Blue
+
+function change_URL() {
+      window.location.replace("https://fa-eqjw-dev10-saasfademo1.ds-fa.oraclepdemos.com/crmUI/faces/FuseWelcome?fndThemeName=Light%20Blue");
+}
+ change_URL()
