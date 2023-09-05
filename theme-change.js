091f6f5d894018848d3b2f4aee6af4d7692b9ba3
valahul <30555482+valahul@users.noreply.github.com>
2023-09-05 14:55:11 +0300

    Update theme-change.js

--- theme-change.js
+++ theme-change.js
@@ -1,1 +1,11 @@
-
+function change_URL(user_role) {
+  switch (user_role) {
+    case "Sales":
+      window.location.replace("http://www.w3schools.com");
+      break;
+    default:
+      console.log("do nothing");
+      break;
+  }
+}
+      change_URL(user_role)
