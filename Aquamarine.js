5a4fadb0b5f85cf2ebfc80d4c8ffc1a8034f8c47
valahul <30555482+valahul@users.noreply.github.com>
2023-09-05 15:20:39 +0300

    Update Aquamarine.js

--- Aquamarine.js
+++ Aquamarine.js
@@ -1,4 +1,4 @@
 function change_URL() {
-      window.location.replace("https://fa-eqjw-dev10-saasfademo1.ds-fa.oraclepdemos.com/crmUI/faces/FuseWelcome?fndThemeName=Light%20Blue");
+      window.location.replace("https://fa-eqjw-dev10-saasfademo1.ds-fa.oraclepdemos.com/crmUI/faces/FuseWelcome?fndThemeName=Aquamarine");
 }
  change_URL()
