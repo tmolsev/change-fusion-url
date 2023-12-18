a9b3f88f8f94a28f2413bb001537c6984fa12e0d
tmolsev <153853620+tmolsev@users.noreply.github.com>
2023-12-18 21:44:52 +0100

    Create SG_theme_set.js

---
+++ SG_theme_set.js
@@ -0,0 +1,27 @@
+function change_URL() {
+  // Check if the script has already run this session
+    
+  //if (sessionStorage.getItem("scriptHasRun")) {
+  //  console.log("The script has already run this session. Skipping...");
+  //  sessionStorage.removeItem("scriptHasRun");
+  //  return;
+  //}
+
+  const fullURL = window.location.href;
+  console.log(fullURL);
+
+  if (fullURL.includes("fndThemeName=SGBrasil")) {
+    console.log("The string contains 'fndThemeName=SGBrasil'");
+  } else {
+    // TM: Add check to avoid setting the theme if the user is in Set Preferences
+    if (!fullURL.includes("itemNode_Tools_Preferences")) {
+      console.log("The string does not contain 'fndThemeName=SGBrasil'");
+      window.location.replace("https://fa-esdr-saasfademo1.ds-fa.oraclepdemos.com/hcmUI/faces/FuseWelcome?fndThemeName=SGBrasil");
+    }
+  }
+
+  // Set the flag indicating the script has run
+  sessionStorage.setItem("scriptHasRun", "true");
+}
+
+change_URL();
