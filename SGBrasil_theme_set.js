3996bdf5105616c9016ec60517ba04ef0232f798
tmolsev <153853620+tmolsev@users.noreply.github.com>
2023-12-18 20:56:25 +0100

    Update SGBrasil_theme_set.js

--- SGBrasil_theme_set.js
+++ SGBrasil_theme_set.js
@@ -13,8 +13,11 @@ function change_URL() {
   if (fullURL.includes("fndThemeName=SGBrasil")) {
     console.log("The string contains 'fndThemeName=SGBrasil'");
   } else {
-    console.log("The string does not contain 'fndThemeName=SGBrasil'");
-    window.location.replace("https://fa-esdr-saasfademo1.ds-fa.oraclepdemos.com/hcmUI/faces/FuseWelcome?fndThemeName=SGBrasil");
+    // TM: Add check to avoid setting the theme if the user is in Set Preferences
+    if (!fullURL.includes("itemNode_Tools_Preferences")) {
+      console.log("The string does not contain 'fndThemeName=SGBrasil'");
+      window.location.replace("https://fa-esdr-saasfademo1.ds-fa.oraclepdemos.com/hcmUI/faces/FuseWelcome?fndThemeName=SGBrasil");
+    }
   }
 
   // Set the flag indicating the script has run
